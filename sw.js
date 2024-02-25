4fcb312b33e3b733d7fdd82bdb479b0e747ad141
LawlieMan <100043085+LawlieMan@users.noreply.github.com>
2024-02-25 18:41:45 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// This is a service worker.
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/marioface.png',
+        // Add other files you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
